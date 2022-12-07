0c208247e5978d9dc48280d55f25ecbb4f20b949
Bethan Williams <bethanw10@gmail.com>
2022-12-07 16:20:43 +0000

    Add Day 6 and 7

---
+++ Day6/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Day6
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var input = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt"));
+            
+            Part1(input);
+            Part2(input);
+        }
+
+        private static void Part1(string input)
+        {
+            IndexOfNDistinctCharacters(input, 4);
+        }
+
+        private static void Part2(string input)
+        {
+            IndexOfNDistinctCharacters(input, 14);
+        }
+
+        private static void IndexOfNDistinctCharacters(string input, int numDistinct)
+        {
+            var packetBuffer = new Queue<char>();
+            for (var i = 0; i < input.Length; i++)
+            {
+                var c = input[i];
+                packetBuffer.Enqueue(c);
+
+                if (packetBuffer.Count > numDistinct)
+                {
+                    packetBuffer.Dequeue();
+                }
+
+                if (packetBuffer.Distinct().Count() == numDistinct)
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Day7/Program.cs
@@ -0,0 +1,148 @@
+﻿using static System.IO.File;
+
+namespace Day7
+{
+    public abstract class Item
+    {
+        public Folder? Parent { get; set; }
+
+        public string Name { get; set; }
+
+        public abstract int Size { get; }
+
+        protected Item(string name) => Name = name;
+    }
+
+    public class Folder : Item
+    {
+        public List<Item> Contents { get; set; } = new();
+
+        public override int Size => TotalSize();
+
+        public Folder(string name) : base(name) { }
+
+        public int TotalSize()
+        {
+            return Contents.Sum(item => item.Size);
+        }
+
+        public List<Folder> GetAllFolders()
+        {
+            var folders = new List<Folder>();
+
+            foreach (var content in Contents.OfType<Folder>())
+            {
+                folders.AddRange(content.GetAllFolders());
+            }
+
+            folders.AddRange(Contents.OfType<Folder>());
+
+            return folders;
+        }
+    }
+
+    public class File : Item
+    {
+        public override int Size { get; }
+
+        public File(string name, int size) : base(name) => Size = size;
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var input = ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt"));
+
+            var commands = input.Split("\r\n");
+
+            Part1(commands);
+            Part2(commands);
+        }
+
+        private static void Part1(IEnumerable<string> terminalOutput)
+        {
+            var root = BuildFileSystem(terminalOutput);
+
+            var allFolders = new List<Folder> { root };
+            allFolders.AddRange(root.GetAllFolders());
+
+            var total = allFolders.Where(folder => folder.Size <= 100000).Sum(folder => folder.Size);
+
+            Console.WriteLine(total);
+        }
+
+        private static void Part2(IEnumerable<string> terminalOutput)
+        {
+            var root = BuildFileSystem(terminalOutput);
+
+            var allFolders = new List<Folder> { root };
+            allFolders.AddRange(root.GetAllFolders());
+
+            const int fileSystemSpace = 70000000;
+            const int updateSpace = 30000000;
+
+            var spaceNeeded = updateSpace - (fileSystemSpace - root.Size);
+
+            var total = allFolders.Where(folder => folder.Size >= spaceNeeded).Min(folder => folder.Size);
+
+            Console.WriteLine(total);
+        }
+
+        private static Folder BuildFileSystem(IEnumerable<string> terminalOutput)
+        {
+            var currentFolder = new Folder("/");
+            var root = currentFolder;
+
+            foreach (var line in terminalOutput.Skip(1))
+            {
+                var words = line.Split(" ");
+
+                if (line.StartsWith('$'))
+                {
+                    var command = words[1];
+
+                    if (command == "cd")
+                    {
+                        var value = words[2];
+
+                        if (value == "..")
+                        {
+                            currentFolder = currentFolder?.Parent;
+                        }
+                        else
+                        {
+                            currentFolder = currentFolder?.Contents
+                                .OfType<Folder>()
+                                .Single(c => c.Name == value);
+                        }
+                    }
+                }
+                else
+                {
+                    if (words[0] == "dir")
+                    {
+                        var name = words[1];
+                        var folder = new Folder(name)
+                        {
+                            Parent = currentFolder
+                        };
+
+                        currentFolder?.Contents.Add(folder);
+                    }
+                    else
+                    {
+                        var size = words[0];
+                        var name = words[1];
+
+                        var file = new File(name, int.Parse(size));
+
+                        currentFolder?.Contents.Add(file);
+                    }
+                }
+            }
+
+            return root;
+        }
+    }
+}
